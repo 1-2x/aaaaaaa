07415cbfc1067871deb9688be4aa88f8412c2e89
1-2x <xlzspe@gmail.com>
2025-08-13 09:47:59 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-fetch('data.json')
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('bio-text').textContent = data.bio;
-        document.getElementById('portrait-img').src = data.portrait;
-    })
-    .catch(error => console.error('Error loading data:', error));
